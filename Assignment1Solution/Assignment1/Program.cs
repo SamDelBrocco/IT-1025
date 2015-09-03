14332a9ded4030dfc3546a68d82734aafb7aa692
S01120395 <S01120395@acad.tri-c.edu>
2015-09-03 19:37:51 -0400

    Added myfirstassignment

---
+++ Assignment1Solution/Assignment1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string firstName;
+            string middleInitial;
+            string lastName;
+            string fullName;
+
+            int age;
+            int heightFeet;
+
+            bool isCitizen;
+            bool canVote;
+
+            double heightInches;
+            double totalHeightCM;
+
+
+
+            System.Console.Write("What is your first name? ");
+            firstName = System.Console.ReadLine();
+
+            System.Console.Write("What is your middle initial? ");
+            middleInitial = System.Console.ReadLine();
+
+            System.Console.Write("What is your last name? ");
+            lastName = System.Console.ReadLine();
+
+            System.Console.WriteLine("Your full name is " + firstName + " " + middleInitial + " " + lastName + ".");
+            fullName = System.Console.ReadLine();
+
+
+
+
+        }
+    }
+}
