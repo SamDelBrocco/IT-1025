ab26ecf2c4dc6d5ab1a1e8d745c65e5c747b8a25
S01120395 <S01120395@acad.tri-c.edu>
2015-09-03 21:20:54 -0400

    Asking for help on double values

--- Assignment1Solution/Assignment1/Program.cs
+++ Assignment1Solution/Assignment1/Program.cs
@@ -30,12 +30,10 @@
             System.Console.Write("What is your last name? ");
             lastName = System.Console.ReadLine();
 
-            fullName = firstName + middleInitial + lastName
-
-            System.Console.WriteLine("Your full name is " + fullName + ".");
+            System.Console.WriteLine("Your full name is " + firstName + " " + middleInitial + " " + lastName + ".");
             fullName = System.Console.ReadLine();
 
-            /////////
+
 
             System.Console.Write("What is your height in feet? ");
             heightFeet = int.Parse(System.Console.ReadLine());
@@ -43,10 +41,26 @@
             System.Console.Write("How many inches beyond your base height in feet are you? ");
             heightInches = int.Parse(System.Console.ReadLine());
 
-            System.Console.WriteLine("Your total height is " + ((heightFeet * 12)+(heightInches * 2.54)));
-            totalHeightCM = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Your total height in CM is " + ((heightFeet * 12) + (heightInches * 2.54)));
+            totalHeightCM = double.Parse(System.Console.ReadLine());
 
 
+
+            System.Console.WriteLine("What is your age? ");
+            age = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("T or F: I am a US Citizen? ");
+            isCitizen = bool.Parse(System.Console.ReadLine());
+
+            bool OK;
+            OK = ((age >= 18) && (isCitizen = true));
+            canVote = OK == true;
+
+            System.Console.WriteLine(fullName + " can vote.");
+            canVote = bool.Parse(System.Console.ReadLine());
+
+
+            
         }
     }
 }
