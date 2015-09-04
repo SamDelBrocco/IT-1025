95992a61eb486e593463239b4f65bce67a3112f4
S01120395 <S01120395@acad.tri-c.edu>
2015-09-03 20:03:27 -0400

    Added height in CM

--- Assignment1Solution/Assignment1/Program.cs
+++ Assignment1Solution/Assignment1/Program.cs
@@ -30,10 +30,21 @@
             System.Console.Write("What is your last name? ");
             lastName = System.Console.ReadLine();
 
-            System.Console.WriteLine("Your full name is " + firstName + " " + middleInitial + " " + lastName + ".");
+            fullName = firstName + middleInitial + lastName
+
+            System.Console.WriteLine("Your full name is " + fullName + ".");
             fullName = System.Console.ReadLine();
 
+            /////////
 
+            System.Console.Write("What is your height in feet? ");
+            heightFeet = int.Parse(System.Console.ReadLine());
+
+            System.Console.Write("How many inches beyond your base height in feet are you? ");
+            heightInches = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("Your total height is " + ((heightFeet * 12)+(heightInches * 2.54)));
+            totalHeightCM = int.Parse(System.Console.ReadLine());
 
 
         }
