1b720aa64928410b725c60ca6ac4de12ae0a61d7
S01120395 <S01120395@acad.tri-c.edu>
2015-09-29 17:41:29 -0400

    Assignment 2 Complete. rid extra Spouse class

---
+++ assignment2Solution/assignment2/Person.cs
@@ -0,0 +1,80 @@
+﻿namespace assignment2
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public Person Spouse;
+        public static int TotalPersonAge = 0;
+        public static int PersonCount = 0;
+        public static int SpouseCount = 0;
+
+
+        public string GetFullName()
+        {
+            PersonCount++;
+            System.Console.WriteLine("Enter person " + PersonCount + " FirstName: ");
+            FirstName = System.Console.ReadLine();
+            System.Console.WriteLine("Enter person " + PersonCount + " LastName: ");
+            LastName = System.Console.ReadLine();
+
+            return this.FirstName + " " + this.LastName + " ";
+        }
+
+
+        public void PrintNameAndAge()
+        {
+            System.Console.WriteLine("Enter person " + PersonCount + " age: ");
+            Age = int.Parse(System.Console.ReadLine());
+            System.Console.WriteLine(this.FirstName + " " + this.LastName + " " + Age);
+            TotalPersonAge += Age;
+        }
+
+
+        public bool MaritalStatus()
+        {
+            System.Console.WriteLine("Is person married?");
+            bool Married;
+            if (System.Console.ReadLine().ToLower().StartsWith("y"))
+            {
+                Married = true;
+                NewSpouse();
+            }
+            else
+            {
+                Married = false;
+                System.Console.WriteLine("No spouse ");
+            }
+            return Married;
+        }
+
+
+        public void NewSpouse()
+        {
+            Spouse = new Person();
+            SpouseCount++;
+            System.Console.WriteLine("What is their spouse first name? ");
+            Spouse.FirstName = System.Console.ReadLine();
+            System.Console.WriteLine("What is their age? ");
+            Spouse.Age = int.Parse(System.Console.ReadLine());
+            TotalPersonAge += Spouse.Age;
+        }
+
+
+        public void SpouseNameAndAge()
+        {
+            System.Console.WriteLine(Spouse.FirstName + " " + LastName + " " + Spouse.Age);
+        }
+
+
+        public double AverageAge()
+        {
+            System.Console.WriteLine("The average age is: ");
+            return TotalPersonAge / (PersonCount + SpouseCount);
+        }
+
+
+    }
+
+}
---
+++ assignment2Solution/assignment2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person P1 = new Person();
+            P1.GetFullName();
+            P1.PrintNameAndAge();
+            P1.MaritalStatus();
+
+
+            Person P2 = new Person();;
+            P2.GetFullName();
+            P2.PrintNameAndAge();
+            P2.MaritalStatus();
+
+
+            Person P3 = new Person();
+            P3.GetFullName();
+            P3.PrintNameAndAge();
+            P3.MaritalStatus();
+
+
+            System.Console.WriteLine("Average age of all people is " + P3.AverageAge());
+            System.Console.WriteLine("Press any key to continue...");
+            System.Console.ReadLine();
+
+        }
+    }
+}
