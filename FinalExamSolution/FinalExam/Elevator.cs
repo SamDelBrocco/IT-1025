46934c47d155d50f42240c4d4d19958ba2322b0b
S01120395 <S01120395@acad.tri-c.edu>
2015-12-07 16:52:51 -0500

    FinalExamSolution

---
+++ FinalExamSolution/FinalExam/Elevator.cs
@@ -0,0 +1,40 @@
+﻿namespace FinalExam
+{
+    public class Elevator
+    {
+        private double MaxWeight;
+        private Passenger[] Occupants;
+       
+
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            Occupants = new Passenger[maxOccupants];
+            MaxWeight = maxWeight;
+        }
+
+        public void AddOccupant(Passenger passenger, int index)
+        {
+            Occupants[index] = passenger;
+        }
+
+        public double GetCurrentWeight()
+        {
+            return Passenger.SumOfWeight;
+        }
+
+        public bool IsOverMaxCapacity()
+        {
+
+            if (GetCurrentWeight() >= MaxWeight)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
